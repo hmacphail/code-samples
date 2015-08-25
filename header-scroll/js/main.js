91f229b21eb335c8c4cfd4e63f5aad954f1c142b
Heather MacPhail <hmacphail@gmail.com>
2015-08-24 18:53:39 -0700

    Sticky scroll wrapper class css test

--- header-scroll/js/main.js
+++ header-scroll/js/main.js
@@ -19,7 +19,7 @@ var stickyHeaders = (function() {
       });
 
       window_obj.on("scroll", function(e) {
-        console.log(e);
+        //console.log(e);
         _whenScrolling();
       });
       //window_obj.off("scroll.stickies").on("scroll.stickies", function() {
@@ -41,10 +41,12 @@ var stickyHeaders = (function() {
             nextStickyPosition = nextSticky.data('originalPosition') - thisSticky.data('originalHeight');
 
         thisSticky.addClass("fixed");
+        
+        //thisSticky.css("position", "fixed");
+        //thisSticky.css("transform", "translate(0,0)");
 
         if (nextSticky.length > 0 && thisSticky.offset().top >= nextStickyPosition) {
-          thisSticky.addClass("absolute").css("transform", "translate(0," + (nextStickyPosition) + "px)");
-          //thisSticky.addClass("absolute").css("top", nextStickyPosition);
+          thisSticky.addClass("absolute").css("transform", "translate(0," + (nextStickyPosition - stickyPosition) + "px)");
         }
 
       } else {
@@ -52,11 +54,17 @@ var stickyHeaders = (function() {
         var prevSticky = stickies_obj.eq(i - 1);
 
         thisSticky.removeClass("fixed");
+        //thisSticky.css("position", "relative");
+        //thisSticky.css("transform", "none");
 
-        if (prevSticky.length > 0 && window_obj.scrollTop() <= thisSticky.data('originalPosition') - thisSticky.data('originalHeight')) {
-
+        if (prevSticky.length > 0 && window_obj.scrollTop() <= thisSticky.data('originalPosition') - thisSticky.data('originalHeight')
+            && window_obj.scrollTop() > prevSticky.data('originalPosition')) { //} - prevSticky.data('originalHeight')) {
           prevSticky.removeClass("absolute").removeAttr("style");
+          //css("position", "fixed").
+          //prevSticky.css("transform", "none");//"translate(0," + window.pageYOffset + "px)");
+
         }
+
       }
     });
   };
